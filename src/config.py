cc8bd5e682ddd54f5f7c47fadd8743bb5c5a797e
juliofreire-collab <juliofreire750@gmail.com>
2026-06-22 16:13:23 -0300

    Add src/config.py

---
+++ src/config.py
@@ -0,0 +1,19 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8981755295:AAF_BaXcWlUuasIfDoao3787lJNMJMCiUd4")
+CHANNEL_ID = os.getenv("CHANNEL_ID", "@avivamentoad")
+GROUP_ID = os.getenv("GROUP_ID", "-1002695823149")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+
+PALAVROES = [
+    "merda", "porra", "caralho", "fdp", "viado", "puta", "vadia", "desgraça",
+    "idiota", "imbecil", "corno", "buceta", "cu", "foder", "filho da puta",
+    "seu merda", "vai se foder", "vai tomar no", "arrombado", "babaca",
+    "bosta", "cuzão", "inferno", "satanás", "diabo", "demônio"
+]
+
+HORARIOS_CANAL_MEDIA = ["09:00", "19:00"]
+HORARIOS_VERSICULOS = ["07:00", "13:00", "21:00"]
+HORARIO_REGRAS_GRUPO = 4
+
+TIMEZONE = "America/Sao_Paulo"
